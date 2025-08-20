9a7e15f7aa45f0cac1f49257c2d8f1c315ccb67a
SLM <sebastian.loaizam@uqvirtual.edu.co>
2025-08-19 19:24:34 -0500

    Add Bienvenido and Util modules with mostrar_mensaje function

---
+++ compilado/bienvenido_04.exs
@@ -0,0 +1,10 @@
+defmodule Bienvenido do
+
+  def mostrar_mensaje() do
+    "Función pública "
+    |> Util.mostrar_mensaje()
+  end
+
+end
+
+Secuencial.mostrar_mensaje()
---
+++ compilado/util.ex
@@ -0,0 +1,6 @@
+defmodule Util do
+  def mostrar_mensaje(mensaje) do
+    mensaje
+    |> IO.puts()
+  end
+end
